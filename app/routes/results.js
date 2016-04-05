a6243a639f450ee83b70f80a3fd42891828433af
Michael Lor and Sean Peerenboom <michael+sean@>
2016-04-05 16:56:36 -0700

    Added completed files.

--- app/routes/results.js
+++ app/routes/results.js
@@ -1,3 +1,4 @@
+
 import Ember from 'ember';
 
 export default Ember.Route.extend({
@@ -5,12 +6,26 @@ export default Ember.Route.extend({
    var url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&q='+ params.zip +"trailer" +'&key=AIzaSyCcAsKzbpibRq6QFIQ5oqVzPlegI9nwMnU';
    console.log(url);
    return Ember.$.getJSON(url).then(function(responseJSON) {
-     console.log(responseJSON);
-     console.log(responseJSON)
      console.log(responseJSON.items[0].id.videoId)
-
-     var correctId = responseJSON.items[0].id.videoId;
-     return correctId;
+    var channelArray =[];
+    for(var i=0; i < 5; i++){
+      console.log("hello");
+     channelArray.push(responseJSON.items[i].snippet.channelId);
+   }
+     return channelArray;
+   }).then(function(channelArray){
+    //  console.log(channelArray);
+    var nameArray = [];
+    console.log(nameArray);
+     channelArray.forEach (function (channel) {
+       var channelUrl = 'https://www.googleapis.com/youtube/v3/channels?part=statistics%2Csnippet&id=' + channel + ' &key=AIzaSyCcAsKzbpibRq6QFIQ5oqVzPlegI9nwMnU'
+        return Ember.$.getJSON(channelUrl).then(function(responseJSON){
+        nameArray.push(responseJSON.items[0].snippet)
+        nameArray.push(responseJSON.items[0].statistics)
+         console.log(nameArray);
    });
-  }
+ })
+ return nameArray;
+})
+ }
 });
